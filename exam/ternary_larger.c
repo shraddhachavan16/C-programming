6bd9df86ecc39a969f7b65b9653f65a94a28f5f6
shraddhachavan16 <shraddhaschavan16@gmail.com>
2025-09-26 22:23:29 +0530

    Add new source code and output files

---
+++ exam/ternary_larger.c
@@ -0,0 +1,8 @@
+#include<stdio.h>
+int main()
+{
+    int n1,n2;
+    printf("enter the two integer\n");
+    scanf("%d%d",&n1,&n2);
+    (n1>n2)?printf("n1 is larger",n1):printf("n2 is larger",n2);
+}
