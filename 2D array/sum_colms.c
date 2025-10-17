853e1900aa18147c218e93d5ce1221d02d1680a8
shraddhachavan16 <shraddhaschavan16@gmail.com>
2025-10-17 20:45:03 +0530

    17-10-25

---
+++ 2D array/sum_colms.c
@@ -0,0 +1,31 @@
+#include<stdio.h>
+int main()
+{
+    int i,j;
+    int rows,colms;
+    printf("entr the rows:");
+    scanf("%d",&rows);
+    printf("entr the colms:");
+    scanf("%d",&colms);
+    int matrix[rows][colms];
+    printf("enter the element of matrix:");
+    for(i=0;i<rows;i++)
+    {
+        for(j=0;j<colms;j++)
+        {
+            scanf("%d",&matrix[i][j]);
+        }
+    }
+ 
+    for(j=0;j<colms;j++)
+    {
+        int sum=0;
+       for(i=0;i<rows;i++)
+        {
+            sum=sum+matrix[i][j];
+        }
+        printf("Sum of colms %d = %d\n", j+ 1, sum);
+    }
+    return 0;
+
+}
