6014dbf83a362c5f0aa49e40fdbdc697a80f48a7
shraddhachavan16 <shraddhaschavan16@gmail.com>
2025-10-29 23:26:00 +0530

    add

---
+++ assignment/right2.c
@@ -0,0 +1,18 @@
+#include<stdio.h>
+int main()
+{
+    int n,i=0;
+    printf("enter the n:");
+    scanf("%d",&n);
+    while(i<=n)
+    {
+         int j=1;
+        while(j<=n-i)
+        {
+            printf("%d",j);
+            j++;
+        }
+        printf("\n");
+        i++;
+    }
+}
