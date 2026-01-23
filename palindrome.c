3c56ac7a677352b7bd2cde644a029a71d801776f
shraddhachavan16 <shraddhaschavan16@gmail.com>
2026-01-23 23:13:27 +0530

    Add files via upload

--- palindrome.c
+++ palindrome.c
@@ -1,15 +1,16 @@
 #include<stdio.h>
 int main()
 {
-    int n,x,temp;
+    int MAX=MAX/10,MIN=MIN/10;
     int ans=0;
-    printf("enter the number:");
-    scanf("%d",&n);
-    while(x>0)
+    int x=0;
+    while(x!=0)
     {
-        temp=x%10;
-        ans=ans*10+temp;
-        x=x/10;
-    }
-    return ans;
+        if(MAX<ans||MIN>ans)
+        {
+            ans=ans*10+(x%10);
+            x=x/10;
+        }
+        printf(ans);
+}
 }
