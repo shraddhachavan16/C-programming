05c356644a8419f604f8aeb894864bba624c1358
shraddhachavan16 <shraddhaschavan16@gmail.com>
2026-02-02 23:05:07 +0530

    Add files via upload

---
+++ prime1.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+int main()
+{
+    int n, i, flag = 1;
+    printf("Enter a number: ");
+    scanf("%d", &n);
+    if (n <= 1)
+        flag = 0;
+    for (i = 2; i < n; i++)
+    {
+        if (n % i == 0)
+        {
+            flag = 0;
+            break;
+        }
+    }
+
+    if (flag == 1)
+        printf("Prime number");
+    else
+        printf("Not a prime number");
+
+    return 0;
+}
