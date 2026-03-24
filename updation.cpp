b384b93a1c629468b309c5f5e296b933368910ce
shraddhachavan16 <shraddhaschavan16@gmail.com>
2026-03-24 21:03:49 +0530

    Add files via upload

---
+++ updation.cpp
@@ -0,0 +1,14 @@
+#include<iostream>
+using namespace std;
+int main(){
+    int x=5;
+    cout<<x<<endl;
+    x=9;
+    cout<<x<<endl;
+    x=x+2;
+    cout<<x<<endl;
+    x=x-3;
+    cout<<x<<endl;
+    x=x*5;
+    cout<<x<<endl;
+}
