5c47930384b2995427f7da1e9422012f830767be
Shraddha <shraddhaschavan16@gmail.com>
2026-06-09 22:23:14 +0530

    9-6-26

---
+++ twosum.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+int main()
+{
+    int a,b;
+    int target=9;
+    printf("enter valuse of a and b:");
+    scanf("%d%d",&a,&b);
+    if(a + b == target)
+        printf("True\n");
+    else
+        printf("False\n");
+
+    return 0;
+}
