25c6eabe04fc02d422144a54286c9814fd115aea
Shraddha <shraddhaschavan16@gmail.com>
2026-07-04 23:17:21 +0530

    07-04-26

---
+++ structure/car.c
@@ -0,0 +1,34 @@
+#include <stdio.h>
+
+struct Car
+{
+    char company[30];
+    char model[30];
+    int year;
+    float price;
+};
+
+int main()
+{
+    struct Car c;
+
+    printf("Enter Company Name: ");
+    scanf("%s", c.company);
+
+    printf("Enter Model Name: ");
+    scanf("%s", c.model);
+
+    printf("Enter Manufacturing Year: ");
+    scanf("%d", &c.year);
+
+    printf("Enter Price: ");
+    scanf("%f", &c.price);
+
+    printf("\n----- Car Details -----\n");
+    printf("Company : %s\n", c.company);
+    printf("Model   : %s\n", c.model);
+    printf("Year    : %d\n", c.year);
+    printf("Price   : %.2f\n", c.price);
+
+    return 0;
+}
