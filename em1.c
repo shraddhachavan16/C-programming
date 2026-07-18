420bd8faa982aa7bcef77685d5d2e6c4c3ecc4b8
Shraddha <shraddhaschavan16@gmail.com>
2026-07-18 21:24:39 +0530

    18-07-26

---
+++ em1.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+
+struct Student {
+    int roll;
+    char name[30];
+};
+
+void display(struct Student *s) {
+    printf("\nRoll = %d", s->roll);
+    printf("\nName = %s", s->name);
+}
+
+int main() {
+
+    struct Student s;
+
+    printf("Enter Roll: ");
+    scanf("%d",&s.roll);
+
+    printf("Enter Name: ");
+    scanf("%s",s.name);
+
+    display(&s);
+
+    return 0;
+}
