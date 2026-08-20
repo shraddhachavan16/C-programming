384fd8c942d296751fb0894678101f481b3c2382
shraddhachavan16 <shraddhaschavan16@gmail.com>
2026-08-20 23:11:08 +0530

    Add files via upload

--- student.c
+++ student.c
@@ -1,28 +1,29 @@
 #include <stdio.h>
 
-// define structure
-struct Student {
-    int roll;
+struct Student
+{
+    int rollNo;
     char name[30];
     float marks;
 };
 
-int main() {
-    struct Student s;   // structure variable
+int main()
+{
+    struct Student s;
 
-    printf("Enter roll number: ");
-    scanf("%d", &s.roll);
+    printf("Enter Roll No: ");
+    scanf("%d", &s.rollNo);
 
-    printf("Enter name: ");
-    scanf("%s", s.name);    // no & for string
+    printf("Enter Name: ");
+    scanf("%s", s.name);
 
-    printf("Enter marks: ");
+    printf("Enter Marks: ");
     scanf("%f", &s.marks);
 
-    printf("\n--- Student Details ---\n");
-    printf("Roll  : %d\n", s.roll);
-    printf("Name  : %s\n", s.name);
-    printf("Marks : %.2f\n", s.marks);
+    printf("\n----- Student Details -----\n");
+    printf("Roll No : %d\n", s.rollNo);
+    printf("Name    : %s\n", s.name);
+    printf("Marks   : %.2f\n", s.marks);
 
     return 0;
 }
